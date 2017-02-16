c33add9feb1303745bda0f74bbeedf32677568f8
Andrew Savinykh <github.com@brutsoft.com>
2017-02-17 12:40:28 +1300

    initial commit

---
+++ foo.c
@@ -0,0 +1,254 @@
+/* foo.c
+ * An interpreter for the Foo esoteric programming language
+ * Written by Feky, 2008 */
+ 
+ #include <stdio.h>
+ #include <string.h>
+ #include <unistd.h>
+ 
+ #define ARRAY_SZ 1024
+ #define STACK_SZ 32
+ #define MAX_NEST_LOOPS 8
+ #define MAX_LINE 255
+ 
+ #define ASCII_ZERO 48
+  
+ typedef unsigned short int USHORT;
+ 
+ struct _stack {
+	 USHORT data[STACK_SZ];
+	 int top;
+ };
+ 
+ struct _loop {
+	 USHORT val;
+	 char *p;
+ };
+
+ USHORT getnum (char *); /* something like atoi(), but it's my own ^^ */
+ int push (USHORT, struct _stack *);
+ USHORT pop (struct _stack *);
+	 
+ USHORT getnum (char *p) {
+	 USHORT y = 0;
+	 
+	 while(isdigit(*p)) {
+		 y *= 10;
+		 y += *p - ASCII_ZERO;
+		 p++;
+	 }
+	 
+	 return y;
+ }
+ 
+ int push (USHORT val, struct _stack *stack) {
+	 
+	if(stack->top == STACK_SZ) {
+		fputs("Stack is full.\n", stderr);
+		return -1;
+	}
+	
+	stack->data[stack->top++] = val;
+		 
+	return 0;
+}
+
+ USHORT pop (struct _stack *stack) {
+	 
+	 if(stack->top < 0) {
+		 fputs("Stack is empty.\n", stderr);
+		 return 0;
+	 }
+	 
+	 stack->top--;
+	 
+	 return stack->data[stack->top];
+ }
+ 
+ int main (int argc, char **argv) {
+	FILE *f;
+	int ptr = 0;
+	USHORT array[ARRAY_SZ];
+	struct _stack stack;
+	char line[MAX_LINE];
+	char *p;
+	USHORT div;
+	int lc = 0;
+	struct _loop loops[MAX_NEST_LOOPS];
+	 
+	 if(argc != 2) {
+		 fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		 return -1;
+	 }
+	 
+	 f = fopen(argv[1], "r");
+	 
+	 if(f == NULL) {
+		 perror("fopen");
+		 return -1;
+	 }
+	 
+	 memset(array, 0, ARRAY_SZ);
+	 stack.top = 0;
+	 
+	 while(fgets(line, MAX_LINE, f) != NULL) {
+		 p = line;
+		 while(*p != '\0') {
+			 switch(*p) {
+				 case '"':
+					 p++;
+					 while(*p != '"' && *p != '\0') {
+						 putchar(*p);
+						 p++;
+					 }
+					 p++;
+				 break;
+					 
+				 case '@':
+					 p++;
+				         if(!isdigit(*p)) {
+						 if(push(array[ptr], &stack)){
+							 fclose(f);
+						         return -1;
+						 }
+					 }
+					 else {
+						 if(push(getnum(p), &stack)) {
+							 fclose(f);
+						         return -1;
+						 }
+					 }
+				 break;
+					 
+				 case '<':
+					 p++;
+					 if(ptr == 0)
+						 ptr = STACK_SZ;
+					 else ptr--;
+				 break;
+				 
+				 case '>':
+					 p++;
+					 if(ptr == STACK_SZ)
+						 ptr = 0;
+					 else ptr++;
+				 break;
+					 
+				 case '&':
+					 p++;
+				         if(!isdigit(*p))
+						 array[ptr] = pop(&stack);
+					 else array[ptr] = getnum(p);
+				break;
+					 
+				case '$':
+					p++;
+				        switch(*p) {
+						case 'i':
+							p++;
+							if(!isdigit(*p))
+								printf("%d", array[ptr]);
+							else printf("%d", getnum(p));
+						break;
+							
+						case 'h':
+							p++;
+							if(!isdigit(*p))
+								printf("%x", array[ptr]);
+							else printf("%x", getnum(p));
+						break;
+							
+						case 'c':
+							p++;
+							if(!isdigit(*p))
+								putchar(array[ptr]%256);
+							else putchar(getnum(p)%256);
+						break;
+							
+						default:
+							fputs("Unknown mode for '$'\n", stderr);
+					        break;
+					}
+				break;
+					 
+				case '+':
+					p++;
+				        if(!isdigit(*p))
+						 array[ptr] += pop(&stack);
+					 else array[ptr] += getnum(p);
+				break;
+					 
+				case '-':
+					p++;
+				        if(!isdigit(*p))
+						 array[ptr] -= pop(&stack);
+					 else array[ptr] -= getnum(p);
+				break;
+					 
+				case '*':
+					p++;
+				        if(!isdigit(*p))
+						 array[ptr] *= pop(&stack);
+					 else array[ptr] *= getnum(p);
+				break;
+					 
+				case '/':
+					p++;
+				        if(!isdigit(*p))
+						div = pop(&stack);
+					 else div = getnum(p);
+				        if(div != 0)
+						array[ptr] /= div;
+					else fputs("Only Chuck Norris can divide by zero.\n", stderr);
+				break;
+					
+				case '%':
+					p++;
+				        if(!isdigit(*p))
+						div = pop(&stack);
+					 else div = getnum(p);
+				        if(div != 0)
+						array[ptr] %= div;
+					else fputs("Only Chuck Norris can divide by zero.\n", stderr);
+				break;
+					
+			        case '#':
+					p++;
+				        if(!isdigit(*p))
+						 sleep(pop(&stack));
+					 else sleep(getnum(p));
+				break;        
+					 
+				case '(':
+					p++;
+				        if(lc == MAX_NEST_LOOPS) {
+						fputs("Maximum number of nested loops reached.\n", stderr);
+						fclose(f);
+						return -1;
+					}
+					if(!isdigit(*p))
+						loops[lc].val = pop(&stack);
+					else loops[lc].val = getnum(p);
+					loops[lc].p = p;
+					lc++;
+				break;
+					
+				case ')':
+					p++;
+					if(lc > 0) {
+						if(array[ptr] != loops[lc - 1].val)
+							p = loops[lc - 1].p;
+						else lc--;
+					}
+				break;
+					 
+				default:
+					p++;
+				break;
+			}			
+		 }
+	 }
+	 
+	 fclose(f);
+	 return 0;
+ }
---
+++ foo_readme.txt
@@ -0,0 +1,58 @@
+Foo
+
+Foo is an esoteric programming language, created by Feky in 2008.
+It is inspired mainly by Brainfuck, and is Turing-complete (since Brainfuck is).
+
+Memory
+
+Foo programs are supplied with a one-dimensional array of X elements (cells, whatever),
+where each element can hold a value from 0 to 65535.
+The array pointer wraps around when it exceeds the number X, and points to
+the first element in the array.
+All elements in the array are initialized to zero on the start of the program.
+There's also a stack to help with arithmetic operations and various copying operations,
+which can also have a custom number (X) of elements, and the value of each can
+vary from 0 to 65535. Conditional jumps (loops) are also controlled
+using a stack, that is limited to the number of maximum nested loops.
+
+Operators
+
+Foo is capable of doing basic and more complex arithmetic operations,
+measuring the time (in seconds, and larger units), controlling the flow
+of execution and outputting values.
+Foo programs end when the interpreter reaches EOF.
+
+" - Everything between these will be printed to stdout (or somewhere else).
+& - Set the value of the currently selected cell in the array to some other value.
+     If followed numbers, like '&55', the cell will be set to 55. If not, it will be
+     set to a popped value from the stack.
+@ - Similar to the '&' operator, but this one pushes values on the stack.
+< - Decrement the array pointer by one
+> - Increment the array pointer by one
+$ - Print out a single value. There are 3 modes - print as an integer (i),
+     hexadecimal integer (h) and ASCII character (c). If the mode isn't
+     specified, a standard Foo interpreter will show and error and continue execution.
+     For example, '$h' will print the current cell in hex, and '$c100' will print 'd'.
++, -, *, /, % - Arithmetic operators. They are always applied to the currently
+                   pointed element in the array.
+		   For example, '+10' will add 10 to the current element, and
+		   '/' will divide the element with a value popped from the stack.
+# - Pause the execution of the program (sleep) for specified seconds. If no number is
+      given after '#', it get the value of the current cell and sleep that much.
+( - Beginning of a loop. If followed by a number, the loop will be executed
+    as long as the value of the current cell is different than the number specified.
+    If there's no number after '(', the loop will execute while the cell is not equal to zero.
+) - Conditional jump (end of a loop). Jumps back to the last '(' if the current cell isn't equal
+    to the value on the top of the loop stack, or continues the program execution normally.
+
+Examples
+
+Hello, World!: "Hello, World!"
+
+Double a number: &256*2$i
+
+Countdown loop: &10(0#1-1$i$c10)"boom!"$c10
+
+Infinite loop: (1)
+
+Negative numbers: &30@50-@&65535-+1$i
